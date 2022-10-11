f3a5955f43a238d1f46cdcb896189993ed7e4bf8
Long <62803507+leesuby@users.noreply.github.com>
2022-10-11 16:44:56 +0700

    add constant

--- FlickerLong/Global/Constant.swift
+++ FlickerLong/Global/Constant.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import UIKit
 
 class Constant{
     
+    class Logo {
+        static var imageName = "LogoNoBackground"
+        static var logoRotate : CGFloat = .pi / 42
+    }
+    
+    class BackgroundOnboarding {
+        static var imageName = "BackgroundOnboarding"
+    }
+    
+    class Description {
+        static var font : UIFont = .NunitoSemiBold(size: 18)!
+        static var color : UIColor = .white
+        static var textAlightment : NSTextAlignment = NSTextAlignment.center
+    }
 }
