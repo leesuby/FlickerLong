327548e827480a8267b9218eeb1b4e169ce536c7
Long <62803507+leesuby@users.noreply.github.com>
2022-10-28 17:32:46 +0700

    refactor post method

--- FlickerLong/Views/Upload/Post/PostViewController.swift
+++ FlickerLong/Views/Upload/Post/PostViewController.swift
@@ -9,37 +9,11 @@ import UIKit
 import objectiveflickr
 
 class PostViewController: UIViewController, ListAlbumViewControllerDelegate, OFFlickrAPIRequestDelegate {
-    var listRequest : [OFFlickrAPIRequest] = []
-    var uploadedPicture : Int = 0
     var choosenAlbum : AlbumModel? = nil
-    var flagUploadAlbum : Bool = false
-    
-    func flickrAPIRequest(_ inRequest: OFFlickrAPIRequest!, didCompleteWithResponse inResponseDictionary: [AnyHashable : Any]!) {
-        print(inResponseDictionary)
-        guard let photoId = inResponseDictionary["photoid"] else{
-            return
-        }
-        
-        let id : String = (photoId as! [String : String])["_text"]!
-        let request : OFFlickrAPIRequest = OFFlickrAPIRequest.init(apiContext: Constant.ObjCContext)
-        request.delegate = self
-        
-        request.callAPIMethod(withPOST: "flickr.photosets.addPhoto", arguments: ["api_key" : "e3e9d23e495da9bf5c0f1a0a63d5be66", "photoset_id" : "\(choosenAlbum!.id!)", "photo_id" : "\(id)"])
-        listRequest.append(request)
-        
-        
-    }
-    
-    func flickrAPIRequest(_ inRequest: OFFlickrAPIRequest!, didFailWithError inError: Error!) {
-        print(inError!)
-    }
-    
-    func flickrAPIRequest(_ inRequest: OFFlickrAPIRequest!, imageUploadSentBytes inSentBytes: UInt, totalBytes inTotalBytes: UInt) {
-    }
-    
     var listImage : [UIImage]!
     var collectionView : UICollectionView!
     var postView : PostView!
+    var repo : Repository = Repository()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,97 +45,14 @@ class PostViewController: UIViewController, ListAlbumViewControllerDelegate, OFF
         let title : String = (self.postView.title.text == "" ? "No Title" : self.postView.title.text)!
         let description : String = (self.postView.description.text == "" ? "No Description" : self.postView.description.text)!
         
-        for image in listImage{
-            let request : OFFlickrAPIRequest = OFFlickrAPIRequest.init(apiContext: Constant.ObjCContext)
-            request.delegate = self
-            
-            let inputStream : InputStream = InputStream.init(data: image.jpegData(compressionQuality: 0.1)!)
-            request.uploadImageStream(inputStream, suggestedFilename: "foo.jpg", mimeType: "image/jpeg", arguments: [
-                "is_public" : "1",
-                "title" : title,
-                "description" : description
-            ])
-            listRequest.append(request)
+        repo.uploadImage(listImage: listImage, title: title, description: description)
+        DispatchQueue.main.async {
+            self.navigationController?.popToRootViewController(animated: true)
+            self.tabBarController?.selectedIndex = 2
         }
         
     }
     
-    public func uploadPhotosURLs1(lobjImageToUpload:UIImage)
-    {
-        let secret =  Constant.UserSession.userOAuthSecret
-        //where secret is 7e5cfde9b0023627
-        let api_key = ProcessInfo.processInfo.environment["API_KEY"]!
-        let auth_token = Constant.UserSession.userOAuthToken
-        
-        let imageData = lobjImageToUpload.jpegData(compressionQuality: 1)
-        
-        let uploadSig = "\(secret)_key\(api_key)_token\(auth_token)"
-        let request = NSMutableURLRequest()
-        let url = "https://up.flickr.com/services/upload/"
-        request.url = URL(string: url)!
-        request.httpMethod = "POST"
-        
-        
-        let boundary = String("---------------------------7d44e178b0434")
-        request.addValue(" multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        
-        let body:NSMutableData = NSMutableData()
-        body.append("\r\n--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
-        
-        body.append("Content-Disposition: form-data; name=\"api_key\"\r\n\r\n".data(using: String.Encoding.utf8)!)
-        body.append("\(api_key)\r\n".data(using: String.Encoding.utf8)!)
-        body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
-        body.append("Content-Disposition: form-data; name=\"auth_token\"\r\n\r\n".data(using: String.Encoding.utf8)!)
-        body.append("\(auth_token)\r\n".data(using: String.Encoding.utf8)!)
-        
-        body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
-        body.append("Content-Disposition: form-data; name=\"api_sig\"\r\n\r\n".data(using: String.Encoding.utf8)!)
-        body.append("\(uploadSig)\r\n".data(using: String.Encoding.utf8)!)
-        body.append("--\(boundary)\r\n".data(using: String.Encoding.utf8)!)
-        body.append(String("Content-Disposition: form-data; name=\"photo\"; filename=\"photo.jpg\"\r\n").data(using: String.Encoding.utf8)!)
-        body.append("Content-Type: image/jpeg\r\n\r\n".data(using: String.Encoding.utf8)!)
-        
-        body.append(imageData!)
-        body.append("\r\n--\(boundary)--\r\n".data(using: String.Encoding.utf8)!)
-        
-        request.httpBody = Data(body.subdata(with: NSRange(location: 0, length: body.length)))
-        
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: request as URLRequest,
-                                    completionHandler: {(data, response, error) in
-            if let error = error {
-                print(error)
-            }
-            if let data = data{
-                print("data =\(data)")
-            }
-            if let response = response {
-                print("url = \(response.url!)")
-                print("response = \(response)")
-                let httpResponse = response as! HTTPURLResponse
-                print("response code = \(httpResponse.statusCode)")
-                print("DATA = \(String(describing: data))")
-                //if you response is json do the following
-                do{
-                    let resultJSON = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions())
-                    let arrayJSON = resultJSON as! NSArray
-                    for value in arrayJSON{
-                        let dicValue = value as! NSDictionary
-                        for (key, value) in dicValue {
-                            print("key = \(key)")
-                            print("value = \(value)")
-                        }
-                    }
-                    
-                }catch _{
-                    print("Received not-well-formatted JSON")
-                }
-            }
-        })
-        task.resume()
-    }
     
     @objc func albumDidTapped(){
         let vc = ListAlbumViewController()
@@ -172,7 +63,9 @@ class PostViewController: UIViewController, ListAlbumViewControllerDelegate, OFF
     func returnChosenAlbum(album: AlbumModel) {
         postView.chooseAlbum(title: album.title)
         self.choosenAlbum = album
+        repo.albumId = album.id
         self.navigationItem.rightBarButtonItem?.isEnabled = true
+        
     }
 }
 
